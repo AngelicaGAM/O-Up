71aeb68363257035e7a8b98a976f1c6cb83c8e54
Angelica Arellanes Merino <160300749@ucaribe.edu.mx>
2018-03-09 13:12:59 -0500

    sum a + b
    Entrada
    La entrada consiste en dos enteros, a y b, separados por un espacio en blanco.
    
    Salida
    Tu salida debe ser un solo número entero, la suma de a y b.

---
+++ sum.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   sum.cpp
+ * Author: angelica
+ *
+ * Created on 9 de marzo de 2018, 12:12 PM
+ */
+
+#include <cstdlib>
+#include<stdio.h>
+
+
+int main(int argc, char** argv) {
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("%d + %d = %d",a,b,c);
+    return 0;
+}
+
